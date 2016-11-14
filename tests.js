7b82c79715c5ca67dcdcac97b0ca13371c9bac9a
egavr <job.egavr@yandex.ru>
2016-11-14 20:46:10 +0300

    feat: add 'mocha-plugin'

--- tests.js
+++ tests.js
@@ -1,6 +1,19 @@
 module.exports = {
+    plugins: [
+        'mocha'
+    ],
+
     env: {
         mocha: true
+    },
+
+    rules: {
+        'mocha/no-exclusive-tests': 'error',
+        'mocha/no-global-tests': 'error',
+        'mocha/no-identical-title': 'error',
+        'mocha/no-pending-tests': 'error',
+        'mocha/no-return-and-callback': 'error',
+        'mocha/no-sibling-hooks': 'error'
     },
 
     globals: {
